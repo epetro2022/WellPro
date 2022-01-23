0dae6d05c79ecaa720a942e174f82cafa6c6b71f
uus khusni <uuskhusni@gmail.com>
2022-01-23 16:34:11 +0700

    tamabh cahrt di analog status

--- LowisChartApp/lw/GroupAnalogStatus.cs
+++ LowisChartApp/lw/GroupAnalogStatus.cs
@@ -12,6 +12,7 @@ using LowisChartApp.model;
 using DevExpress.XtraGrid.Views.Grid;
 using DevExpress.XtraGrid;
 using System.Collections;
+using DevExpress.XtraCharts;
 // ok
 namespace LowisChartApp.lw
 {
@@ -82,6 +83,72 @@ namespace LowisChartApp.lw
             {
                 DataRow row = gvLowis.GetDataRow(i);
                 Globals.ForeignKey = row[1].ToString().Trim();
+            }
+            string tmp;
+        }
+
+        private void gvLowis_RowClick(object sender, RowClickEventArgs e)
+        {
+            //Int32[] selectedRowHandles = gvLowis.GetSelectedRows();
+            //ArrayList rows = new ArrayList();
+            //for (int i = 0; i < selectedRowHandles.Length; i++)
+            //{
+            //    int selectedRowHandle = selectedRowHandles[i];
+            //    if (selectedRowHandle >= 0)
+            //        rows.Add(gvLowis.GetDataRow(selectedRowHandle));
+            //}
+
+            //for (int i = 0; i < rows.Count; i++)
+            //{
+            //    DataRow row = rows[i] as DataRow;
+            //    string tmpdata = row["FacilityWellName"].ToString();
+            //}
+
+            string FacilityWellName = (sender as GridView).GetFocusedRowCellValue("FacilityWellName").ToString();
+            string AnalogPointDescription = (sender as GridView).GetFocusedRowCellValue("AnalogPointDescription").ToString();
+            dbCon.clearCache();
+            Dictionary<string, object> paras = new Dictionary<string, object>();
+            paras.Add("@wellname", FacilityWellName);
+            paras.Add("@Analog_Desc", AnalogPointDescription);
+            DataSet ds = dbCon.execprosedure("dbo.[prosedur_getdataanalogtrendnew]", paras);
+            if (ds.Tables.Count < 1)
+            {
+                //MessageBox.Show(ds.Tables[0].Rows.Count.ToString());
+                MessageBox.Show("Database execute timeout, chart analog trend can't be show", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                Series series1 = new Series("Series 1", ViewType.Spline);
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    //Console.WriteLine(row["ImagePath"]);
+                    series1.Points.Add(new SeriesPoint(row["timestamp"], row["VALUE"]));
+
+                }
+                //gdLowis.DataSource = ds.Tables[0];
+                //// Add the series to the chart.
+                chartanalogtrend.Series.Add(series1);
+                //lineChart.Series.Add(series2);
+
+                // Set the numerical argument scale types for the series,
+                // as it is qualitative, by default.
+                series1.ArgumentScaleType = ScaleType.DateTime;
+
+                // Access the view-type-specific options of the series.
+                ((LineSeriesView)series1.View).MarkerVisibility = DevExpress.Utils.DefaultBoolean.True;
+                ((LineSeriesView)series1.View).LineMarkerOptions.Kind = MarkerKind.Circle;
+                ((LineSeriesView)series1.View).LineStyle.DashStyle = DashStyle.Solid;
+
+                // Access the type-specific options of the diagram.
+                ((XYDiagram)chartanalogtrend.Diagram).EnableAxisXZooming = true;
+
+                // Hide the legend (if necessary).
+                chartanalogtrend.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+
+                // Add a title to the chart (if necessary).
+                chartanalogtrend.Titles.Add(new ChartTitle());
+                chartanalogtrend.Titles[0].Text = "Analog Trend Wellname : " + FacilityWellName + "-" + AnalogPointDescription;
             }
         }
     }
